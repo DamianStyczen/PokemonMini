524a1343f9c23b75384cbea48a621fd5cd6fd23f
Damian Styczeń <styczen.damian@gmail.com>
2018-08-25 21:13:56 +0200

    Seperate files

--- Pokemon App/js/app.js
+++ Pokemon App/js/app.js
@@ -1,15 +1,5 @@
-const CHARSHEET = document.getElementById("characterSpritesheet");
-const BGSHEET = document.getElementById("backgroundSpritesheet");
-const GRASSSHEET = document.getElementById("grassSpritesheet");
-const WORLDUNIT = 64;
-let red;
-
-if(localStorage.getItem("test")){
-    console.log(localStorage.getItem("test"));
-}
-const board = document.getElementById("board");
-const bCtx = board.getContext('2d');
-// bCtx.drawImage(CHARSHEET, 64, 0, CHARSIZE, CHARSIZE, 64, 64, CHARSIZE, CHARSIZE);
+import {Board} from "./board";
+import {Character} from "./character";
 
 let boardFields = [
     ["grass","grass", "grass", "grass","grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
@@ -23,129 +13,23 @@ let boardFields = [
     ["grass","grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
     ["grass","grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
 ];
-const generateBackground = ()=>{
-    for(let i = 0; i < boardFields.length; i++){
-        for(let j = 0; j < boardFields[i].length; j++){
-            switch(boardFields[i][j]){
-                case "grass":
-                bCtx.drawImage(GRASSSHEET, 0, 0, 32, 32, WORLDUNIT*j, WORLDUNIT*i, 64, 64);
-                break;
-                case "bush":
-                bCtx.drawImage(BGSHEET, 64, 0, 64, 64, WORLDUNIT*j, WORLDUNIT*i, 64, 64);
-                break;
-                default:
-                bCtx.drawImage(GRASSSHEET, 0, 0, 32, 32, WORLDUNIT*j, WORLDUNIT*i, 64, 64);
-                break;
-            }
-                       
-        }
 
-    }
-}
+const canvas = document.getElementById("board");
+const bCtx = canvas.getContext('2d');
+const WORLDUNIT = 64;
 
+let red = new Character(7*WORLDUNIT, 4*WORLDUNIT, WORLDUNIT, boardFields);
+let board = new Board(boardFields, WORLDUNIT);
 
 const update = ()=>{
 
     bCtx.clearRect(0,0,board.width,board.height);
-    generateBackground();
-    red.draw();
+    board.generateBackground(bCtx);
+    red.draw(bCtx);
 }
 
-class Character{
-    constructor(positionX, positionY, spriteSheet){
-        this.width = WORLDUNIT;
-        this.height = WORLDUNIT;
-        this.positionX = positionX;        
-        this.positionY = positionY;
-        this.spriteSheet = spriteSheet;
-        this.isMoving = false;
-        this.direction = "down";
-        this.spriteX = 0;
-        this.spriteY = 0;
-        this.movementSpeed = 4;
-        this.nextStep = [this.positionX, this.positionY];
-        this.animationFrame = 0;
-    }
-    draw(){
-        this.animationFrame ++;
-        if(this.isMoving){            
-            this.move();
-        }
-        bCtx.drawImage(this.spriteSheet, this.spriteX, this.spriteY, this.width, this.width, this.positionX, this.positionY, this.width, this.height);
-    }
-    rotate(newDirection){
-        if(!this.isMoving){
-            this.direction = newDirection;
-            switch(this.direction){
-                case "down":
-                    this.spriteY = 0*this.width;
-                    break;
-                case "left":
-                    this.spriteY = 1*this.width;
-                    break;
-                case "right":
-                    this.spriteY = 2*this.width;
-                    break;
-                case "up":
-                    this.spriteY = 3*this.width;
-                    break;
-            }
-        }
-    }
-    startMoving(){
-        if(!this.isMoving){
-            this.isMoving = true;
-            switch(this.direction){
-                case "down":
-                    this.nextStep[1] += this.height;
-                    break;
-                case "left":
-                    this.nextStep[0] -= this.width;
-                    break;
-                case "right":
-                    this.nextStep[0] += this.width;
-                    break;
-                case "up":
-                    this.nextStep[1] -= this.height;
-                    break;
-            }
-        }
-    }
-    move(){
-        switch(this.direction){
-            case "down":
-                this.positionY += this.movementSpeed;
-                break;
-            case "left":
-                this.positionX -= this.movementSpeed;
-                break;
-            case "right":
-                this.positionX += this.movementSpeed;
-                break;
-            case "up":
-                this.positionY -= this.movementSpeed;
-                break;
-        }
-        if(this.positionX == this.nextStep[0] && this.positionY == this.nextStep[1]){
-            this.isMoving = false;
-            console.log("Stepped on: ", boardFields[this.positionY/WORLDUNIT][this.positionX/WORLDUNIT]);
-            localStorage.setItem("positionX", this.positionX);
-            localStorage.setItem("positionY", this.positionY);
-            localStorage.setItem("direction", this.direction);
-        }
-    }
-
-}
-// console.log(localStorage.getItem("positionX"));
-// if(localStorage.getItem("positionX") != null){ 
-//     console.log("its not null");
-//     red = new Character(localStorage.getItem("positionX"), localStorage.getItem("positionY"), CHARSHEET);
-//     red.direction = localStorage.getItem("direction");
-// }
-// else{
-    red = new Character(7*WORLDUNIT, 4*WORLDUNIT, CHARSHEET);
-// }
 window.addEventListener('keydown', function(event){
+    event.preventDefault();
     switch(event.which){
         case 37:
         red.rotate("left");
@@ -164,7 +48,7 @@ window.addEventListener('keydown', function(event){
         red.startMoving();
         break;
     }
-    
+
 });
 
 let gameInterval = setInterval(()=>{
---
+++ Pokemon App/js/out.js
@@ -0,0 +1,125 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./js/app.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./js/app.js":
+/*!*******************!*\
+  !*** ./js/app.js ***!
+  \*******************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nvar _board = __webpack_require__(/*! ./board */ \"./js/board.js\");\n\nvar _character = __webpack_require__(/*! ./character */ \"./js/character.js\");\n\nvar boardFields = [[\"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\"], [\"grass\", \"grass\", \"bush\", \"bush\", \"bush\", \"bush\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\"], [\"grass\", \"grass\", \"bush\", \"bush\", \"bush\", \"bush\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\"], [\"grass\", \"grass\", \"bush\", \"bush\", \"bush\", \"bush\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\"], [\"grass\", \"grass\", \"bush\", \"bush\", \"bush\", \"bush\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\"], [\"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\"], [\"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\"], [\"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\"], [\"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\"], [\"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\", \"grass\"]];\n\nvar canvas = document.getElementById(\"board\");\nvar bCtx = canvas.getContext('2d');\nvar WORLDUNIT = 64;\n\nvar red = new _character.Character(7 * WORLDUNIT, 4 * WORLDUNIT, WORLDUNIT, boardFields);\nvar board = new _board.Board(boardFields, WORLDUNIT);\n\nvar update = function update() {\n\n    bCtx.clearRect(0, 0, board.width, board.height);\n    board.generateBackground(bCtx);\n    red.draw(bCtx);\n};\n\nwindow.addEventListener('keydown', function (event) {\n    event.preventDefault();\n    switch (event.which) {\n        case 37:\n            red.rotate(\"left\");\n            red.startMoving();\n            break;\n        case 39:\n            red.rotate(\"right\");\n            red.startMoving();\n            break;\n        case 38:\n            red.rotate(\"up\");\n            red.startMoving();\n            break;\n        case 40:\n            red.rotate(\"down\");\n            red.startMoving();\n            break;\n    }\n});\n\nvar gameInterval = setInterval(function () {\n    update();\n}, 33.33);\n\n//# sourceURL=webpack:///./js/app.js?");
+
+/***/ }),
+
+/***/ "./js/board.js":
+/*!*********************!*\
+  !*** ./js/board.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n    value: true\n});\n\nvar _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nvar Board = function () {\n    function Board(boardFields, baseUnit) {\n        _classCallCheck(this, Board);\n\n        this.boardFields = boardFields;\n        this.BGSHEET = document.getElementById(\"backgroundSpritesheet\");\n        this.GRASSSHEET = document.getElementById(\"grassSpritesheet\");\n        this.WORLDUNIT = baseUnit;\n    }\n\n    _createClass(Board, [{\n        key: \"generateBackground\",\n        value: function generateBackground(context) {\n            for (var i = 0; i < this.boardFields.length; i++) {\n                for (var j = 0; j < this.boardFields[i].length; j++) {\n                    switch (this.boardFields[i][j]) {\n                        case \"grass\":\n                            context.drawImage(this.GRASSSHEET, 0, 0, 32, 32, this.WORLDUNIT * j, this.WORLDUNIT * i, 64, 64);\n                            break;\n                        case \"bush\":\n                            context.drawImage(this.BGSHEET, 64, 0, 64, 64, this.WORLDUNIT * j, this.WORLDUNIT * i, 64, 64);\n                            break;\n                        default:\n                            context.drawImage(this.GRASSSHEET, 0, 0, 32, 32, this.WORLDUNIT * j, this.WORLDUNIT * i, 64, 64);\n                            break;\n                    }\n                }\n            }\n        }\n    }]);\n\n    return Board;\n}();\n\nexports.Board = Board;\n\n//# sourceURL=webpack:///./js/board.js?");
+
+/***/ }),
+
+/***/ "./js/character.js":
+/*!*************************!*\
+  !*** ./js/character.js ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n    value: true\n});\n\nvar _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nvar Character = function () {\n    function Character(positionX, positionY, baseUnit, boardFields) {\n        _classCallCheck(this, Character);\n\n        this.width = baseUnit;\n        this.height = baseUnit;\n        this.WORLDUNIT = baseUnit;\n        this.positionX = positionX;\n        this.positionY = positionY;\n        this.isMoving = false;\n        this.direction = \"down\";\n        this.spriteX = 0;\n        this.spriteY = 0;\n        this.movementSpeed = 4;\n        this.nextStep = [this.positionX, this.positionY];\n        this.animationFrame = 0;\n        this.boardFields = boardFields;\n    }\n\n    _createClass(Character, [{\n        key: \"draw\",\n        value: function draw(context) {\n            var CHARSHEET = document.getElementById(\"characterSpritesheet\");\n            this.animationFrame++;\n            if (this.isMoving) {\n                this.move();\n            }\n            context.drawImage(CHARSHEET, this.spriteX, this.spriteY, this.width, this.width, this.positionX, this.positionY, this.width, this.height);\n        }\n    }, {\n        key: \"rotate\",\n        value: function rotate(newDirection) {\n            if (!this.isMoving) {\n                this.direction = newDirection;\n                switch (this.direction) {\n                    case \"down\":\n                        this.spriteY = 0 * this.width;\n                        break;\n                    case \"left\":\n                        this.spriteY = 1 * this.width;\n                        break;\n                    case \"right\":\n                        this.spriteY = 2 * this.width;\n                        break;\n                    case \"up\":\n                        this.spriteY = 3 * this.width;\n                        break;\n                }\n            }\n        }\n    }, {\n        key: \"startMoving\",\n        value: function startMoving() {\n            if (!this.isMoving) {\n                this.isMoving = true;\n                switch (this.direction) {\n                    case \"down\":\n                        this.nextStep[1] += this.height;\n                        break;\n                    case \"left\":\n                        this.nextStep[0] -= this.width;\n                        break;\n                    case \"right\":\n                        this.nextStep[0] += this.width;\n                        break;\n                    case \"up\":\n                        this.nextStep[1] -= this.height;\n                        break;\n                }\n            }\n        }\n    }, {\n        key: \"move\",\n        value: function move() {\n            switch (this.direction) {\n                case \"down\":\n                    this.positionY += this.movementSpeed;\n                    break;\n                case \"left\":\n                    this.positionX -= this.movementSpeed;\n                    break;\n                case \"right\":\n                    this.positionX += this.movementSpeed;\n                    break;\n                case \"up\":\n                    this.positionY -= this.movementSpeed;\n                    break;\n            }\n            if (this.positionX == this.nextStep[0] && this.positionY == this.nextStep[1]) {\n                this.isMoving = false;\n                console.log(\"Stepped on: \", this.boardFields[this.positionY / this.WORLDUNIT][this.positionX / this.WORLDUNIT]);\n                localStorage.setItem(\"positionX\", this.positionX);\n                localStorage.setItem(\"positionY\", this.positionY);\n                localStorage.setItem(\"direction\", this.direction);\n            }\n        }\n    }]);\n\n    return Character;\n}();\n\nexports.Character = Character;\n\n//# sourceURL=webpack:///./js/character.js?");
+
+/***/ })
+
+/******/ });
