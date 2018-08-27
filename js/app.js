25589c96edcffde86ce0cace0d73fc891b385619
Damian Styczeń <styczen.damian@gmail.com>
2018-08-27 18:07:26 +0200

    Connected to PokeAPI for pokemon data and sprites

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ import {Battle} from "./battle";
 
 let boardFields = [
     ["grass","grass", "grass", "grass","grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
-    ["grass","grass", "bush", "bush", "bush", "bush", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
-    ["grass","grass", "bush", "bush", "bush", "bush", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
-    ["grass","grass", "bush", "bush", "bush", "bush", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
-    ["grass","grass", "bush", "bush", "bush", "bush", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
-    ["grass","grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
+    ["bush","grass", "grass", "grass", "grass", "grass", "grass", "grass", "bush", "grass", "grass", "grass", "grass", "grass", "grass"],
+    ["bush","grass", "bush", "grass", "bush", "grass", "bush", "grass", "bush", "grass", "grass", "grass", "bush", "bush", "bush"],
+    ["bush","bush", "grass", "grass", "bush", "grass", "bush", "grass", "bush", "bush", "bush", "grass", "bush", "grass", "bush"],
+    ["bush","grass", "bush", "grass", "bush", "grass", "bush", "grass", "bush", "grass", "bush", "grass", "bush", "bush", "bush"],
+    ["bush","grass", "bush", "grass", "bush", "bush", "bush", "grass", "bush", "bush", "bush", "grass", "bush", "grass", "bush"],
     ["grass","grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
     ["grass","grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
     ["grass","grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
@@ -21,7 +21,7 @@ const WORLDUNIT = 64;
 
 let red = new Character(7*WORLDUNIT, 4*WORLDUNIT, WORLDUNIT, boardFields);
 let board = new Board(boardFields, WORLDUNIT);
-let battle = new Battle();
+let battle = new Battle(1, 6);
 
 const update = ()=>{
 
--- js/battle.js
+++ js/battle.js
@@ -1,27 +1,37 @@
 class Battle{
-    constructor(player, enemy){
+    constructor(player, enemyID){
     this.player = player;
-    this.enemy = enemy;
-    //this.getPokemonInfo();
+    $.ajax({
+        url: "https://pokeapi.co/api/v2/pokemon/"+ enemyID +"/"
+    }).done((result)=>{
+        this.enemy = result;
+        console.log(this.enemy);
+        this.enemySprite = new Image();
+        this.enemySprite.src  = this.enemy.sprites.front_default;
+    })
+    $.ajax({
+        url: "https://pokeapi.co/api/v2/pokemon/151/"
+    }).done((result)=>{
+        this.friendly = result;
+        console.log(this.enemy);
+        this.friendlySprite = new Image();
+        this.friendlySprite.src  = this.friendly.sprites.back_default;
+    })
+
+        this.friendlySprite = new Image();
+        this.friendlySprite.src  = "this.friendly.sprites.back_default";
+
     }
     draw(context, baseUnit){
-        let pokemonid = 1;
-        let pokemonSprite = document.getElementById("pokemonSpritesheet");
         let battleCircleSprite = document.getElementById("battleCircleSprite");
         context.drawImage(battleCircleSprite, 0, 0, battleCircleSprite.width, battleCircleSprite.height, baseUnit*6, baseUnit*3, battleCircleSprite.width, battleCircleSprite.height);
         context.drawImage(battleCircleSprite, 0, 0, battleCircleSprite.width, battleCircleSprite.height, baseUnit*0, baseUnit*6, battleCircleSprite.width, battleCircleSprite.height);
-        context.drawImage(pokemonSprite, 0, 0, 64, 64, baseUnit*7, baseUnit*0, 5*64, 5*64);
+        context.drawImage(this.enemySprite, 0, 0, 96, 96, baseUnit*8, baseUnit*0, 5*64, 5*64);
+        context.drawImage(this.friendlySprite, 0, 0, 96, 96, baseUnit*2, baseUnit*3, 5*64, 5*64);
 
 
     }
-    getPokemonInfo(){
-        $.ajax({
-            url: "http://pokeapi.salestock.net/api/v2/pokemon/1"
-        }).done((result)=>{
-            this.enemy = result;
-            console.log(this.enemy);
-        })
-    }
+
 
 }
 
